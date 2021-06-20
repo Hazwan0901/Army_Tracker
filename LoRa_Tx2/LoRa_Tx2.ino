99d341f30290ffc7a6cdf7b2d78e3f3fc9af5f78
hadifikri <hadifikri@gmail.com>
2021-06-20 23:48:41 +0800

    test random delay with busy/success check

--- LoRa_Tx2/LoRa_Tx2.ino
+++ LoRa_Tx2/LoRa_Tx2.ino
@@ -2,7 +2,7 @@
 
 int pot = A1;
 
-int val;
+int counter = 1;
 
 int SyncWord = 0x21;
 
@@ -10,16 +10,19 @@ void setup()
 {
   Serial.begin(9600);
   pinMode(pot, INPUT);
-  while (!Serial);
+  while (!Serial)
+    ;
   Serial.println("LoRa Sender");
 
-  if (!LoRa.begin(433E6)) {
+  if (!LoRa.begin(433E6))
+  {
     Serial.println("Starting LoRa failed!");
-    while (1);
+    while (1)
+      ;
   }
-  LoRa.setSpreadingFactor(12);           // ranges from 6-12,default 7 see API docs
-  LoRa.setSignalBandwidth(62.5E3 );           // for -139dB (page - 112)
-  LoRa.setCodingRate4(8);                   // for -139dB (page - 112)
+  LoRa.setSpreadingFactor(12);     // ranges from 6-12,default 7 see API docs
+  LoRa.setSignalBandwidth(62.5E3); // for -139dB (page - 112)
+  LoRa.setCodingRate4(8);          // for -139dB (page - 112)
   LoRa.setSyncWord(SyncWord);
 }
 
@@ -28,18 +31,28 @@ void loop()
 
   //val = map(analogRead(pot), 0, 1024, 0, 255);
   Serial.println("Sending packet: ");
-  val++;
-  if(val>100){
-    val = 0;
-  }
-  // send packet
-  LoRa.beginPacket();
-  LoRa.print("P2:");
-  LoRa.print(val);
-  LoRa.endPacket();
 
-  Serial.print("Value:");
-  Serial.print(val);
-  Serial.println("");
-  delay(50);
+  // send packet
+  if (LoRa.beginPacket() == 1)
+  {
+    // ready to transmit
+    LoRa.print("P2:");
+    LoRa.print(counter);
+    if (LoRa.endPacket() == 1)
+    {
+      // success
+      Serial.print("P2:");
+      Serial.print(counter);
+      Serial.println("");
+
+      counter++;
+      if (counter > 100)
+      {
+        counter = 1;
+      }
+    }
+    delay(1000);
+    return;
+  }
+  delay(random(1000, 2000));
 }
